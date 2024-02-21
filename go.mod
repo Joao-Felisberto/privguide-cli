1b5a68f5bfd9e293c9d04032fbd439a428a80151
Joao-Felisberto <joaofelisberto@ua.pt>
2024-02-21 21:40:22 +0000

    Fixed schema validation

--- go.mod
+++ go.mod
@@ -2,16 +2,20 @@ module github.com/Joao-Felisberto/devprivops
 
 go 1.21.5
 
-require (
 //	github.com/ghodss/yaml v1.0.0
-	github.com/spf13/cobra v1.8.0
-)
+require github.com/spf13/cobra v1.8.0
 
 require (
 	//	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/santhosh-tekuri/jsonschema v1.2.4
 	github.com/spf13/pflag v1.0.5 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0
 // github.com/yuin/gopher-lua v1.1.1 // indirect
+)
+
+require github.com/xeipuuv/gojsonschema v1.2.0
+
+require (
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 )
--- schema/schema.go
+++ schema/schema.go
@@ -3,10 +3,12 @@ package schema
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
-	"github.com/santhosh-tekuri/jsonschema"
+	// "github.com/santhosh-tekuri/jsonschema"
+	"github.com/xeipuuv/gojsonschema"
 	"gopkg.in/yaml.v2"
 )
 
@@ -41,6 +43,29 @@ func NewTriple(s, p, o string) Triple {
 }
 
 var idCounter int
+
+func convertToJSON(data interface{}) interface{} {
+	switch v := data.(type) {
+	case map[interface{}]interface{}:
+		m := make(map[string]interface{})
+		for key, value := range v {
+			m[fmt.Sprintf("%v", key)] = convertToJSON(value)
+		}
+		return m
+	case []interface{}:
+		if len(v) == 0 {
+			// Empty array represented as an array
+			return []interface{}{}
+		}
+		var convertedArray []interface{}
+		for _, value := range v {
+			convertedArray = append(convertedArray, convertToJSON(value))
+		}
+		return convertedArray
+	default:
+		return data
+	}
+}
 
 func toStringKeys(val interface{}) (interface{}, error) {
 	switch val := val.(type) {
@@ -87,20 +112,8 @@ func ReadYAML(yamlFile string, schemaFile string) (interface{}, error) {
 	}
 
 	if schemaFile != "" {
-		schemaText, err := os.ReadFile(schemaFile)
+		_, err := ValidateYAMLAgainstSchema(yamlFile, schemaFile)
 		if err != nil {
-			return nil, err
-		}
-
-		compiler := jsonschema.NewCompiler()
-		if err := compiler.AddResource("schema.json", strings.NewReader(string(schemaText))); err != nil {
-			return nil, err
-		}
-		schema, err := compiler.Compile("schema.json")
-		if err != nil {
-			return nil, err
-		}
-		if err := schema.ValidateInterface(data); err != nil {
 			return nil, err
 		}
 	}
@@ -108,51 +121,99 @@ func ReadYAML(yamlFile string, schemaFile string) (interface{}, error) {
 	return data, nil
 }
 
-// ValidateYAMLAgainstSchema validates a YAML file against a JSON Schema
-func ValidateYAMLAgainstSchema(yamlFile string, schemaFile string) error {
-	// Read schema
-	schemaText, err := os.ReadFile(schemaFile)
+func ValidateYAMLAgainstSchema(yamlFile string, schemaFile string) (*gojsonschema.Result, error) {
+	// Load JSON schema
+	schemaLoader := gojsonschema.NewReferenceLoader("file://" + schemaFile)
+	schema, err := gojsonschema.NewSchema(schemaLoader)
 	if err != nil {
-		return err
+		log.Fatalf("Failed to load JSON schema: %v", err)
+		return nil, err
 	}
 
-	/*
-		// Read YAML file
-		yamlData, err := os.ReadFile(yamlFile)
-		if err != nil {
-			return err
-		}
-
-		// Unmarshal YAML data
-		var rawData interface{}
-		if err := yaml.Unmarshal(yamlData, &rawData); err != nil {
-			return err
-		}
-		data, err := toStringKeys(rawData)
-		if err != nil {
-			return err
-		}
-	*/
-	data, err := ReadYAML(yamlFile, "")
+	// Load YAML data
+	yamlData, err := os.ReadFile(yamlFile)
 	if err != nil {
-		return err
+		log.Fatalf("Failed to read YAML file: %v", err)
+		return nil, err
 	}
 
-	// Load schema
-	compiler := jsonschema.NewCompiler()
-	if err := compiler.AddResource("schema.json", strings.NewReader(string(schemaText))); err != nil {
-		return err
-	}
-	schema, err := compiler.Compile("schema.json")
+	// Parse YAML data
+	var yamlObj interface{}
+	err = yaml.Unmarshal(yamlData, &yamlObj)
 	if err != nil {
-		return err
-	}
-	if err := schema.ValidateInterface(data); err != nil {
-		return err
+		log.Fatalf("Failed to parse YAML data: %v", err)
+		return nil, err
 	}
 
-	return nil
+	// Convert YAML data to JSON-like structure
+	jsonData := convertToJSON(yamlObj)
+
+	// Validate JSON-like data against JSON schema
+	jsonLoader := gojsonschema.NewGoLoader(jsonData)
+	result, err := schema.Validate(jsonLoader)
+	if err != nil {
+		log.Fatalf("Failed to validate YAML data: %v", err)
+		return nil, err
+	}
+
+	// Print validation result
+	if result.Valid() {
+		fmt.Println("YAML file abides by the schema.")
+	} else {
+		fmt.Println("YAML file does not abide by the schema. Validation errors:")
+		for _, desc := range result.Errors() {
+			fmt.Printf("- %s\n", desc)
+		}
+	}
+
+	return result, nil
 }
+
+// // ValidateYAMLAgainstSchema validates a YAML file against a JSON Schema
+// func ValidateYAMLAgainstSchema(yamlFile string, schemaFile string) error {
+// 	// Read schema
+// 	schemaText, err := os.ReadFile(schemaFile)
+// 	if err != nil {
+// 		return err
+// 	}
+//
+// 	/*
+// 		// Read YAML file
+// 		yamlData, err := os.ReadFile(yamlFile)
+// 		if err != nil {
+// 			return err
+// 		}
+//
+// 		// Unmarshal YAML data
+// 		var rawData interface{}
+// 		if err := yaml.Unmarshal(yamlData, &rawData); err != nil {
+// 			return err
+// 		}
+// 		data, err := toStringKeys(rawData)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	*/
+// 	data, err := ReadYAML(yamlFile, "")
+// 	if err != nil {
+// 		return err
+// 	}
+//
+// 	// Load schema
+// 	compiler := jsonschema.NewCompiler()
+// 	if err := compiler.AddResource("schema.json", strings.NewReader(string(schemaText))); err != nil {
+// 		return err
+// 	}
+// 	schema, err := compiler.Compile("schema.json")
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if err := schema.ValidateInterface(data); err != nil {
+// 		return err
+// 	}
+//
+// 	return nil
+// }
 
 func generateAnonID() string {
 	idCounter++
